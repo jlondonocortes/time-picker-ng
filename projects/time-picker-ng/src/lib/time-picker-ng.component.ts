e89b387006d4e66a0c6068c7afeacf24c2022536
Tuckito <jlondonocortes@gmail.com>
2024-10-17 00:02:56 -0500

    css: se comenta las clases y estilos no utilizados. se crea estilo para los item. html: se actualiza sintaxis @for. se crea elementos items para las horas. ts: se crea las funciones eventoscroll, actualizarItemActivo y manejarscrollinfinito (incompleta)

--- projects/time-picker-ng/src/lib/time-picker-ng.component.ts
+++ projects/time-picker-ng/src/lib/time-picker-ng.component.ts
@@ -8,7 +8,7 @@ import { NgFor } from '@angular/common';
   templateUrl: './time-picker-ng.component.html',
   styleUrls: ['./time-picker-ng.component.css']
 })
-export class TimePickerNgComponent {
+export class TimePickerNgComponent{
   hours: number[] = Array.from({length: 12}, (_, i) => i + 1);
   minutes: number[] = Array.from({length: 60}, (_, i) => i);
   selectedHour: number = 1;
@@ -24,4 +24,46 @@ export class TimePickerNgComponent {
     const hour = document.getElementById('minute');
     console.log(hour);
   }
+
+  eventoScroll(event: any){
+    //obtenemo elementos con la clase .time-column
+    const elemento = document.querySelector('.time-column');
+    //llamamos función para definir elemento activo
+    this.actualizarItemActivo(elemento);
+    //llamamos function para manjera el scroll infinito
+    this.manejarScrollInfinito(elemento);
+  }
+
+  actualizarItemActivo(elemento: any){    
+    //obtenemo posicion y tamaño del elemento
+    const objetoDom = elemento?.getBoundingClientRect(); 
+    //calculamo posición centro
+    const posCentro = (objetoDom?.top??0) + (objetoDom?.height??0) /2;
+    //recorremo elemento dentro de la clase .time-column
+    Array.from(elemento?.children??[]).forEach((item:any) => { 
+      //obtenemo posicion y tamaño de los elementos hijos
+      const itemDom = item.getBoundingClientRect();
+      //calculamos posición centro del elemento hijo
+      const itemPosCentro = itemDom.top + itemDom.height/2;
+      //se verifica si elemento hijo es el que esta en el centro del elemento contenedor padre
+      //se le agrega la clase .active si se cumple la condición, si no se quita.
+      if(Math.abs(itemPosCentro - posCentro)< 63/2){
+        item.classList.add('active');
+      }else{
+        item.classList.remove('active');
+      }
+    })
+  }
+
+  manejarScrollInfinito(elemento: any){
+    const scrollTop = elemento.scrollTop;
+    const scrollHeight = elemento.scrollHeight;
+    const clientHeight = elemento.clientHeight;
+    const totalHeight = 63 * this.hours.length;
+
+    if(scrollTop + clientHeight > scrollHeight - totalHeight){
+      //continuar
+    }
+  }
+
 }
