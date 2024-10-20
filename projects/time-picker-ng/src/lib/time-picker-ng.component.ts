bae18a9ca1db9f9cae26bbf1672e6754269dccf6
Tuckito <jlondonocortes@gmail.com>
2024-10-19 20:43:27 -0500

    Se termina la version preliminar del componente.

--- projects/time-picker-ng/src/lib/time-picker-ng.component.ts
+++ projects/time-picker-ng/src/lib/time-picker-ng.component.ts
@@ -1,4 +1,4 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { Component,  EventEmitter,  Input, Output, OnInit,  ViewEncapsulation } from '@angular/core';
 import { NgFor } from '@angular/common';
 
 @Component({
@@ -6,21 +6,91 @@ import { NgFor } from '@angular/common';
   standalone: true,
   imports: [NgFor],
   templateUrl: './time-picker-ng.component.html',
-  styleUrls: ['./time-picker-ng.component.css']
+  styleUrls: ['./time-picker-ng.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
-export class TimePickerNgComponent{
-  hours: number[] = Array.from({length: 12}, (_, i) => i + 1);
-  minutes: number[] = Array.from({length: 60}, (_, i) => i);
-  selectedHour: number = 1;
-  selectedMinute: number = 0;
+export class TimePickerNgComponent implements OnInit{
+  @Input() time = "12";
+  @Output() selectedTimeEvent = new EventEmitter<string>();
   period: string = 'AM';
-  private itemHeight: number = 63;
-  private container: any; 
+  private scrollTimeout:any;
+  
+  ngOnInit(): void {
+    console.log(this.time);
 
-  ngAfterViewInit(): void {
-    this.container = document.querySelector('.time-column');
-    //Call to function initializeScroll
-    this.initializeScroll();
+    const hourPicker = document.getElementById('hourPicker');
+    const minutePicker = document.getElementById('minutePicker');
+    this.generateTimeOptions(hourPicker,0,Number(this.time),1);
+    this.generateTimeOptions(minutePicker,0,59,1);
+
+    [hourPicker, minutePicker].forEach(picker => {
+      this.updateSelection(picker);
+      this.centerSelectedItem(picker);
+    })
+    
+    this.updateSelectedTime();
+  }
+
+  generateTimeOptions(container: any, start: number, end:number, step:number){
+    for(let i = start; i<= end; i+= step){
+      const item = document.createElement('div');
+      item.className = 'item';
+      item.textContent = i.toString().padStart(2,'0');
+      container.appendChild(item);
+    }
+  }
+
+  updateSelection(container: any){
+    const items = container.querySelectorAll('.item');
+    const containerRect = container.getBoundingClientRect();
+    const containerMiddle = containerRect.top + containerRect.height / 2;
+
+    let closestItem: any = null;
+    let minDistance = Infinity;
+
+    items.forEach((item:any) =>{
+      const itemRect = item.getBoundingClientRect();
+      const itemMiddle = itemRect.top + itemRect.height / 2;
+      const distance = Math.abs(itemMiddle - containerMiddle);
+
+      if(distance < minDistance){
+        minDistance = distance;
+        closestItem = item;
+      }
+
+      item.classList.remove('active');
+    });
+
+    if(closestItem){
+      closestItem?.classList.add('active');
+    }
+  }
+
+  centerSelectedItem(container:any){
+    const selectedItem = container.querySelector('.active');
+    if(selectedItem){
+      const containerHeight = container.clientHeight;
+      const itemHeight = selectedItem.clientHeight;
+      const scrollTop = selectedItem.offsetTop - (containerHeight / 2) + (itemHeight / 2);
+      container.scrollTop = scrollTop;
+    }
+  }
+
+  updateSelectedTime(){
+    const selectedHour = document.querySelector('#hourPicker .active')?.textContent || '00';
+    const selectedMinuted = document.querySelector('#minutePicker .active')?.textContent || '00';
+    const selectedTime = document.getElementById('selectedTime');
+
+    selectedTime?.setHTMLUnsafe(`${selectedHour}:${selectedMinuted}`);
+  }
+
+  eventScroll(event: any){
+    clearTimeout(this.scrollTimeout);
+    this.scrollTimeout = setTimeout(() => {
+      this.updateSelection(event.srcElement)
+      this.centerSelectedItem(event.srcElement);
+      this.updateSelectedTime();
+    }, 100);    
   }
   
   setPeriod(newPeriod: string) {
@@ -29,69 +99,10 @@ export class TimePickerNgComponent{
   }
 
   saveTime(){
-    const hour = document.getElementById('minute');
-    console.log(hour);
-  }
-
-  /*createElement(){
-    this.hours.forEach(item =>{
-      const i
-    })
-  }
-
-  createItem(el:number){
-    const item = document.createElement('div');
-
-  }*/
-
-
-  eventoScroll(event: any){
-    //obtenemo elementos con la clase .time-column
-    const elemento = document.querySelector('.time-column');
-    //llamamos función para definir elemento activo
-    this.actualizarItemActivo(elemento);
-    //llamamos function para manjera el scroll infinito
-    this.manejarScrollInfinito(elemento); 
-  }
-
-  actualizarItemActivo(elemento: any){    
-    //obtenemo posicion y tamaño del elemento
-    const objetoDom = elemento?.getBoundingClientRect(); 
-    //calculamo posición centro
-    const posCentro = (objetoDom?.top??0) + (objetoDom?.height??0) /2;
-    //recorremo elemento dentro de la clase .time-column
-    Array.from(elemento?.children??[]).forEach((item:any) => { 
-      //obtenemo posicion y tamaño de los elementos hijos
-      const itemDom = item.getBoundingClientRect();
-      //calculamos posición centro del elemento hijo
-      const itemPosCentro = itemDom.top + itemDom.height/2;
-      //se verifica si elemento hijo es el que esta en el centro del elemento contenedor padre
-      //se le agrega la clase .active si se cumple la condición, si no se quita.
-      if(Math.abs(itemPosCentro - posCentro)< this.itemHeight/2){
-        item.classList.add('active');
-      }else{
-        item.classList.remove('active');
-      }
-    })
-  }
-
-  manejarScrollInfinito(elemento: any){
-    const scrollTop = elemento.scrollTop;
-    const scrollHeight = elemento.scrollHeight;
-    const clientHeight = elemento.clientHeight;
-    const totalHeight = 63 * this.hours.length;
-
-    if(scrollTop + clientHeight > scrollHeight - totalHeight){
-      //continuar
-    }else if(scrollTop < totalHeight){
-      
-    }
-  }
-
-  initializeScroll(){ 
-    const totalHeight = this.itemHeight * this.hours.length;
-    const initialScrollPosition = (totalHeight - this.container.clientHeight) / 2 + this.itemHeight / 2;
-    this.container.scrollTop = initialScrollPosition-90;
+    const hour = document.getElementById('selectedTime');
+    const selectedTime = hour?.textContent||"00:00";
+    console.log(hour?.textContent);
+    this.selectedTimeEvent.emit(selectedTime);
   }
 
 }
