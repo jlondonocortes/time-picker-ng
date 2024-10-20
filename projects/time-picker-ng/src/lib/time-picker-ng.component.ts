84658f29866190da5c5e57c0d5b2cbbde5be7259
Tuckito <jlondonocortes@gmail.com>
2024-10-20 15:50:44 -0500

    Se comenta código

--- projects/time-picker-ng/src/lib/time-picker-ng.component.ts
+++ projects/time-picker-ng/src/lib/time-picker-ng.component.ts
@@ -10,28 +10,30 @@ import { NgFor } from '@angular/common';
   encapsulation: ViewEncapsulation.None
 })
 export class TimePickerNgComponent implements OnInit{
-  @Input() time = "12";
-  @Output() selectedTimeEvent = new EventEmitter<string>();
+  @Input() time = "12"; //Formato de hora 12 o 24
+  @Output() selectedTimeEvent = new EventEmitter<string>(); //Respuesta hora selecciona
   period: string = 'AM';
   private scrollTimeout:any;
   
   ngOnInit(): void {
-    console.log(this.time);
-
+    //Se obtiene los elementos que contiene los item de hora y minutos.
     const hourPicker = document.getElementById('hourPicker');
     const minutePicker = document.getElementById('minutePicker');
-    this.generateTimeOptions(hourPicker,0,Number(this.time),1);
-    this.generateTimeOptions(minutePicker,0,59,1);
 
+    this.generateTimeOptions(hourPicker,0,Number(this.time),1); // Se genera los div de horas
+    this.generateTimeOptions(minutePicker,0,59,1); //se genera los div de minuntos
+
+    //se recorre las horas y minutos; se llama las functionas updateSelection y centerSelectedItem
     [hourPicker, minutePicker].forEach(picker => {
       this.updateSelection(picker);
       this.centerSelectedItem(picker);
     })
-    
+    //Se llama a la funcion updateSelectedTime
     this.updateSelectedTime();
   }
 
   generateTimeOptions(container: any, start: number, end:number, step:number){
+    //Se crea elementos de clase item
     for(let i = start; i<= end; i+= step){
       const item = document.createElement('div');
       item.className = 'item';
@@ -41,13 +43,18 @@ export class TimePickerNgComponent implements OnInit{
   }
 
   updateSelection(container: any){
+    //Se obtiene todo los elemento con la clase .item
     const items = container.querySelectorAll('.item');
+    //Se obtiene posición y dimensiones del conteneror    
     const containerRect = container.getBoundingClientRect();
+    //Se calcular la mitad del contenedor
     const containerMiddle = containerRect.top + containerRect.height / 2;
 
+    //Se inicializa las variables closestItem y minDistance
     let closestItem: any = null;
     let minDistance = Infinity;
 
+    //Se recorre los elementos y se calcula que elemento esta en el centro para asignarle la clase .active
     items.forEach((item:any) =>{
       const itemRect = item.getBoundingClientRect();
       const itemMiddle = itemRect.top + itemRect.height / 2;
@@ -67,7 +74,9 @@ export class TimePickerNgComponent implements OnInit{
   }
 
   centerSelectedItem(container:any){
+    //Se obtiene el elemento que tiene la clase .active
     const selectedItem = container.querySelector('.active');
+    //Si existe elemento, se centra en el contenedor
     if(selectedItem){
       const containerHeight = container.clientHeight;
       const itemHeight = selectedItem.clientHeight;
@@ -77,6 +86,7 @@ export class TimePickerNgComponent implements OnInit{
   }
 
   updateSelectedTime(){
+    //se obtiene horas y minutos seleccionado.
     const selectedHour = document.querySelector('#hourPicker .active')?.textContent || '00';
     const selectedMinuted = document.querySelector('#minutePicker .active')?.textContent || '00';
     const selectedTime = document.getElementById('selectedTime');
@@ -95,7 +105,6 @@ export class TimePickerNgComponent implements OnInit{
   
   setPeriod(newPeriod: string) {
     this.period = newPeriod;
-    //this.updateTime();
   }
 
   saveTime(){
